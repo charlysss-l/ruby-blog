b61cf1d732dced0c33d45527e5a7304d559c401a
Charlize Mikaela Nadela <105686280+charlysss-l@users.noreply.github.com>
2026-01-12 14:20:53 +0800

    Add edit and delete

--- article/app/controllers/my_articles_controller.rb
+++ article/app/controllers/my_articles_controller.rb
@@ -1,9 +1,48 @@
 class MyArticlesController < ApplicationController
+  before_action :set_my_article, only: %i[show edit update destroy]
+
   def index
     @my_articles = MyArticle.all
   end
+
   def show
-    @my_articles = MyArticle.find(params[:id])
+  end
+
+  def new
+    @my_article = MyArticle.new
+  end
+
+  def create
+    @my_article = MyArticle.new(my_article_params)
+    if @my_article.save
+      redirect_to my_articles_path, notice: "Article created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @my_article.update(my_article_params)
+      redirect_to @my_article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @my_article.destroy
+    redirect_to my_articles_path
   end
   
+  private
+  def set_my_article
+    @my_article = MyArticle.find(params[:id])
+  end
+
+  def my_article_params
+    params.require(:my_article).permit(:title, :body)
+  end
 end
